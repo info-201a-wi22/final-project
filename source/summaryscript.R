10ac9e9054f88dfe811ac0ea2195f8346a188217
trieudung199 <trieudung199@gmail.com>
2022-02-18 20:02:56 -0800

    add summary script

---
+++ source/summaryscript.R
@@ -0,0 +1,42 @@
+library(dplyr)
+library(tidyr)
+
+
+crime_data <- read_csv("/Users/rtdt/documents/info-201-exercises/final-project/data/seattle_crime_data.csv")
+incomebylocationdata <- read_csv("/Users/rtdt/documents/info-201-exercises/final-project/data/income_by_location.csv")
+
+# Finds the total amount of crime for each crime category and filters the top 10.
+count <- crime_data %>%
+  count(Offense.Parent.Group) %>%
+  top_n(10, n)
+
+# Filters the crime_data to only show the top 10 crime data categories.
+crime_data_top_10 <- crime_data %>%
+  filter(Offense.Parent.Group %in% count$Offense.Parent.Group)
+
+#Most committed crime groups in 2019
+crime_data$Year <- format(as.Date(crime_data$Offense.Start.DateTime, format="%d/%m/%Y"),"%Y")
+crime_group_2019 <- crime_data %>%
+  filter(Year == 2019) %>%
+  group_by(Offense.Parent.Group) %>%
+  count() 
+most_committed_crime_group_2019 <- crime_group_2019 %>%
+  filter(n == max(n)) %>%
+  
+
+#Least committed crime group in 2019
+least_committed_crime_group_2019 <- crime_group_2019 %>%
+  filter(n == min(n)) %>%
+  pull(Offense.Parent.Group)
+  
+#Census tract with the highest household income by race in 2019
+highest_income_2019 <- incomebylocationdata %>%
+  filter(Year == 2019) %>%
+  filter(`Household Income by Race` == max(`Household Income by Race`)) %>%
+  pull(Geography)
+
+#Census tract with the lowest household income by race in 2019
+highest_income_2019 <- incomebylocationdata %>%
+  filter(Year == 2019) %>%
+  filter(`Household Income by Race` == min(`Household Income by Race`)) %>%
+  pull(Geography)
