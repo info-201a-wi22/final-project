39ff517765046f67ee3b1be8831ab53921581c22
trieudung199 <trieudung199@gmail.com>
2022-02-20 21:26:07 -0800

    Add summary and table paragraphs

--- source/summaryscript.R
+++ source/summaryscript.R
@@ -1,8 +1,9 @@
 library(dplyr)
 library(tidyr)
+library(tidyverse)
 
 crime_data <- read_csv("../data/seattle_crime_data.csv")
-incomebylocationdata <- read_csv("../data/income_by_location.csv")
+income_by_location_data <- read_csv("../data/income_by_location.csv")
 
 crime_data_counts <- crime_data %>%
   mutate(year = format(as.Date(crime_data$Offense.Start.DateTime, format="%d/%m/%Y"),"%Y")) %>%
@@ -33,7 +34,7 @@ highest_income_location_2019 <- income_by_location_data %>%
   pull(Geography)
 
 # Highest income in Seattle 2019
-highest_income_location_2019 <- income_by_location_data %>%
+highest_income_2019 <- income_by_location_data %>%
   filter(Year == 2019) %>%
   filter(`Household Income by Race` == max(`Household Income by Race`)) %>%
   pull(`Household Income by Race`)
@@ -45,7 +46,7 @@ lowest_income_location_2019 <- income_by_location_data %>%
   pull(Geography)
 
 # Lowest income in Seattle 2019
-lowest_income_location_2019 <- income_by_location_data %>%
+lowest_income_2019 <- income_by_location_data %>%
   filter(Year == 2019) %>%
   filter(`Household Income by Race` == min(`Household Income by Race`)) %>%
   pull(`Household Income by Race`)
