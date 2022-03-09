dbc41a386e8407b97f56a4b73a0fb52c269fef28
trieudung199 <trieudung199@gmail.com>
2022-03-09 08:04:57 -0800

    Add summary heading

--- source/shiny/panels/summary_panel.R
+++ source/shiny/panels/summary_panel.R
@@ -1,4 +1,13 @@
 summary_panel <- tabPanel(
   "Summary", 
-  titlePanel("Summary and Takeaways")
+  titlePanel("Summary and Takeaways"),
+  h3("Summary"),
+  p("The below diagram was created by the New York Times to illustrate the
+    increasing level of inequality in the US."),
+  # An image `img()` to display. This content should have a `src` attribute of
+  # "inequality.png" (which will refer to the file in the `www/` folder).
+  img(src = "inequality.png"),
+  h3("Takeaways"),
+  p('Click on the "Growth Chart" tab to see an interactive diagram.')
 )
+
